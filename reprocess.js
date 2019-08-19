58d5e0adecd2f72bd60a4a3a92aa6595d3162ad8
Zachery2008 <46687719+Zachery2008@users.noreply.github.com>
2019-08-19 13:34:41 -0700

    delete unuseful comments

--- reprocess.js
+++ reprocess.js
@@ -118,40 +118,6 @@ async function upload2Cosmos(assessmentDir){
 }
 
 
-async function deletion(dataDir){
-    fs.readdir(dataDir, (error, fileNames) => {
-        if (error) throw error;
-
-        fileNames.forEach(filename => {
-            // get current file name
-            const name = path.parse(filename).name;
-            // get current file extension
-            const ext = path.parse(filename).ext;
-            // get current file path
-            const filepath = path.resolve(dataDir, filename);
-        
-            // get information about the file
-            fs.stat(filepath, function(error, stat) {
-                if (error) throw error;
-    
-                // check if the current path is a file or a folder
-                const isFile = stat.isFile();  
-                console.log(isFile); 
-                console.log(filepath);
-                
-                if (isFile) {
-                // callback, do something with the file
-                //processFile(filepath, name, ext, stat);
-                fs.unlinkSync(filepath);
-                }
-            });
-            
-        });
-    });
-}
-
-
-
 module.exports = async function executeReprocess(Input){
     return new Promise((resolve, reject) => {
         const AssessmentID = Input.assessmentID;
@@ -199,29 +165,5 @@ module.exports = async function executeReprocess(Input){
     });
 }
 
-/*
-    downloadBlob(containerName, DcaID, AssessmentDir)
-        .then(() => {
-            downloadBlob(containerName, CompanyID, AssessmentDir)
-            .then(() => {
-                runMatlab(MatlabArgs) 
-                .then(() => {
-                    upload2Cosmos(AssessmentDir)
-                    .then(() => {
-                        fsExtra.remove(AssessmentDir,(err) =>{
-                            if(err){
-                                callback(true);
-                            }
-                            else{
-                                callback(false);
-                            }
-                        })
-                })
-            })
-        })
-    });
-}
-
-*/
 
 
